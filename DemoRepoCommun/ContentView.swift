81ee1fe14b72c697d0bd8b1a790c4e5c91040761
iliasse-e <elabouyi.iliasse@gmail.com>
2020-05-06 11:53:17 +0200

    Initial Commit

---
+++ DemoRepoCommun/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DemoRepoCommun
+//
+//  Created by EL ABOUYI on 06/05/2020.
+//  Copyright © 2020 AFP. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
